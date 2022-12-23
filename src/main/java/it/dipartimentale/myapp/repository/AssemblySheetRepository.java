518acb1691eef1599c95b87b44f587a9abdfccf8
Federico Di Maio <f.dimaio@cineca.it>
2022-12-23 08:37:38 +0100

    fix repo

--- src/main/java/it/dipartimentale/myapp/repository/AssemblySheetRepository.java
+++ src/main/java/it/dipartimentale/myapp/repository/AssemblySheetRepository.java
@@ -2,6 +2,7 @@ package it.dipartimentale.myapp.repository;
 
 import it.dipartimentale.myapp.dto.utils.AssemblySheet;
 import it.dipartimentale.myapp.repository.commonRepository.IRepository;
+import org.springframework.stereotype.Repository;
 
 
 /**
@@ -9,5 +10,6 @@ import it.dipartimentale.myapp.repository.commonRepository.IRepository;
  * @date : 22/12/2022
  * @Project : tesi
  */
+@Repository
 public interface AssemblySheetRepository extends IRepository<AssemblySheet, String> {
 }
--- src/main/java/it/dipartimentale/myapp/repository/DepartementHeadRepository.java
+++ src/main/java/it/dipartimentale/myapp/repository/DepartementHeadRepository.java
@@ -2,6 +2,7 @@ package it.dipartimentale.myapp.repository;
 
 import it.dipartimentale.myapp.dto.DepartmentHeadDto;
 import it.dipartimentale.myapp.repository.commonRepository.IRepository;
+import org.springframework.stereotype.Repository;
 
 
 /**
@@ -9,5 +10,6 @@ import it.dipartimentale.myapp.repository.commonRepository.IRepository;
  * @date : 22/12/2022
  * @Project : tesi
  */
+@Repository
 public interface DepartementHeadRepository  extends IRepository<DepartmentHeadDto, String> {
 }
